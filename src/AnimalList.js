7d831287ebbc36b3344f29a1e1e060c2ddb2046c
marijanikolovski <m.kuzeljevic@gmail.com>
2022-11-01 23:12:16 +0100

    Date of birth functionality

--- src/AnimalList.js
+++ src/AnimalList.js
@@ -7,6 +7,7 @@ const AnimalList = () => {
         {id: 3, species: 'bird', name: 'Pevacica', dateOfBirth: new Date()},
         {id: 4, species: 'cat', name: 'Mica', dateOfBirth: new Date()},
         {id: 5, species: 'snake', name: 'Jeca', dateOfBirth: new Date()},
+        {id: 6, species: 'dog', name: 'Dzeki', },
     ]);
 
   return (
@@ -24,7 +25,7 @@ const AnimalList = () => {
                 <tr key={animal.id}>
                     <th>{animal.species}</th>
                     <th>{animal.name}</th>
-                    <th>{animal.dateOfBirth.toDateString()}</th>
+                    <th>{animal.dateOfBirth ? animal.dateOfBirth.toDateString() : 'Nepoznat'}</th>
                 </tr>
             ))}
         </tbody>
